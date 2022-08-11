be83a171fa64e471db223608b969897f167716bb
Zeshan Munir <zmunir93@gmail.com>
2022-08-11 02:53:59 -0400

    fixed css linking to ejs issue. created router.js and render.js files

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-const {MongoClient} = require('mongodb');
 const mongoose = require('mongoose');
 const ejs = require("ejs")
 const bodyParser = require("body-parser")
 const path = require('path')
 
-dotenv.config({path:'config.env'})
-
-const PORT = process.env.PORT || 8080
 const app=express();
 
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT || 8080
+
+// logs requests
 app.use(morgan('tiny'));
 
 // parse request to body-parser
@@ -21,48 +21,12 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 
 // load assets
-app.use('/css', express.static(path.resolve(__dirname, "assets.css")))
-app.use('/img', express.static(path.resolve(__dirname, "assets.img")))
-app.use('/js', express.static(path.resolve(__dirname, "assets.js")))
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')))
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')))
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')))
 
-// mongoose.connect("mongodb+srv://zmunir93:Chicken12@crud-app.0e40d.mongodb.net/crud", { useNewUrlParser: true }, { useUnifiedTopology: true });
-
-// const todoSchema = {
-//     note: String
-// }
-
-// third "todo" is to match the collection in db. so Mongoose.model(name, [Schema], [collection]). if no collection is passed, name will be used and pluralized.
-// const Note = mongoose.model("todo", todoSchema, "todo")
-
-app.get("/", function(req, res) {
-
-    res.render('index')
-    // Note.find({}, function(err, note){
-    //     res.render('index', {
-    //         newNote: note
-    //     })
-    // })
-})
-
-// app.post("/", function(req, res) {
-//     let newNote = new Note({
-//        note: req.body.note
-//     });
-//     newNote.save();
-//     res.redirect("/");
-// })
-
-// if(window.location.pathname == "/"){
-//     $ondelete = $(".form.button");
-//     $ondelete.click(function(){
-//         var id = $(this).attr("data-id")
-
-//     })
-// }
-
-// app.delete("/", function(req, res) {
-//     newNote.delete()
-// })
+// load routers
+app.use('/', require('./server/routes/router'))
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
