92c26ea8bd7affda84dad31330592dce20e975fc
Zeshan Munir <zmunir93@gmail.com>
2022-08-16 17:20:11 -0400

    fixed index.js localhost link. changed to heroku url

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,7 +15,7 @@ $("#update_user").submit(function(event){
     console.log(data);
 
     var request = {
-        "url": `http://localhost:3000/api/users/${data.id}`,
+        "url": `https://zee-crud-app.herokuapp.com/api/users/${data.id}`,
         "method": "PUT",
         "data": data
     }
@@ -32,7 +32,7 @@ if(window.location.pathname == "/"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url": `http://localhost:3000/api/users/${id}`,
+            "url": `https://zee-crud-app.herokuapp.com/api/users/${id}`,
             "method": "Delete"
         }
 
